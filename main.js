f364744ee6cfec74aa916f6d16b5a88ef9915124
Lucas Fernando Correa de Souza <lucasfernandocorreadesouza@MacBook-Pro-de-lfcsvt.local>
2018-11-22 18:02:18 +0100

    searchbar working

--- main.js
+++ main.js
@@ -6,16 +6,18 @@ function getData() {
             main(books)
         })
 }
-window.onload = () => getData()
+window.onload = () => {
+    getData()
+    search()
+}
 
 function main(books) {
     createBooks(books)
-    console.log(books)
-  
 }
 
-function createBooks(books){
+function createBooks(books) {
     var bookstore = document.getElementById('bookstore')
+
 
     books.forEach(book => {
         book
@@ -24,24 +26,24 @@ function createBooks(books){
         var backImageContent = book.detalle
         var description = book.descripcion
         var flipContainer = document.createElement('div')
-       flipContainer.setAttribute('id', 'flip-container')
-       flipContainer.setAttribute('class', 'flip-container')
+        flipContainer.setAttribute('class', 'flip-card')
         var book = document.createElement('div')
-        book.setAttribute('id', 'one-book')
-        book.setAttribute('class', 'flipper')
-        var tittle = document.createElement('p')
+        book.setAttribute('class', 'flip-card-inner')
+        var tittle = document.createElement('h3')
+        tittle.setAttribute('class', 'book-tittle')
         var image = document.createElement('img')
-        image.setAttribute("style", "height: 300; width: 300px")
+        image.setAttribute("class", "front-image")
         image.setAttribute('src', imageContent)
         var bookFront = document.createElement('div')
-        bookFront.setAttribute('id', 'front')
-        bookFront.setAttribute('class', 'book-front')
+        bookFront.setAttribute('class', 'flip-card-front')
         var backImage = document.createElement('img')
-        backImage.setAttribute("style", "height: 300; width: 300px")
+        backImage.setAttribute("class", "back-image")
         var bookBack = document.createElement('div')
         var bookDescript = document.createElement('p')
-        bookBack.setAttribute('id', 'book-back')
-        bookBack.setAttribute('class', 'back')
+        var buttton = document.createElement('button')
+        buttton.setAttribute('type', 'button')
+        buttton.setAttribute('class', 'btn btn-lg btn-primary')
+        bookBack.setAttribute('class', 'flip-card-back')
         backImage.setAttribute('src', backImageContent)
         tittle.innerHTML = tittleText
         bookDescript.innerHTML = description
@@ -49,12 +51,32 @@ function createBooks(books){
         bookBack.appendChild(bookDescript)
         bookFront.appendChild(image)
         bookBack.appendChild(backImage)
+        bookBack.appendChild(buttton)
         book.appendChild(bookFront)
         book.appendChild(bookBack)
         flipContainer.appendChild(book)
         bookstore.appendChild(flipContainer)
-        
+
     })
+
+    search()
 }
 
+function search() {
+    var books = Array.from(document.getElementsByClassName("flip-card"))
+    console.log(books)
+    var searchBar = document.forms['search-books'].querySelector('input')
+    searchBar.addEventListener('keyup', function (e) {
+        var term = e.target.value.toLowerCase();
+        books.forEach(function (book) {
+            let tittle = book.outerText
+            console.log(tittle)
+            if (tittle.toLowerCase().indexOf(term) != -1) {
+                book.style.display = 'block'
+            } else {
+                book.style.display = 'none'
+            }
+        })
+    })
+}
 
